a0c91fd2735042954a74c449b416e5830df89644
José Manuel Chacpa Vizcarra <josechacpa@upeu.edu.pe>
2020-10-26 09:34:13 -0500

    Por fin funciona V:

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'calculator-design.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
+
+
